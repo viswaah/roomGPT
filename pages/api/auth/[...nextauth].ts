8b692cc065efa2afba14824d2d8ef0f3e1c43864
viswaah <vishwajenvish@gmail.com>
2023-04-09 08:56:39 +0530

    fix: protected page routes

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -11,6 +11,10 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
+  pages: {
+    signIn: '/dream',
+    signOut: '/'
+  }
 };
 
 export default NextAuth({
